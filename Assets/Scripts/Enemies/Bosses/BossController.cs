9e43393cf399ba0f67e22fced48d50e415038ac9
Jerry Nordman <jerry.nordman@student.careeria.fi>
2023-05-05 16:42:01 +0300

    Started making a boss controller

--- Assets/Scripts/Enemies/Bosses/BossController.cs
+++ Assets/Scripts/Enemies/Bosses/BossController.cs
@@ -4,15 +4,89 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int currentPhase = 0;
+    [SerializeField] private float timeBetweenAttacks = 3.0f;
+    private float timeSinceLastAttack = 0.0f;
+    private bool isAttacking = false;
+
+    private void Update()
     {
-        
+        if (!isAttacking && Time.time >= timeSinceLastAttack + timeBetweenAttacks)
+        {
+            StartCoroutine(PerformAttack());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator PerformAttack()
     {
-        
+        isAttacking = true;
+
+        // Choose an attack based on the current phase
+        switch (currentPhase)
+        {
+            case 0:
+                // Execute attack 1 or attack 2 for phase 0
+                if (Random.Range(0, 2) == 0)
+                    Phase0Attack1();
+                else
+                    Phase0Attack2();
+                break;
+            case 1:
+                // Execute attack 1 or attack 2 for phase 1
+                if (Random.Range(0, 2) == 0)
+                    Phase1Attack1();
+                else
+                    Phase1Attack2();
+                break;
+            case 2:
+                // Execute attack 1 or attack 2 for phase 2
+                if (Random.Range(0, 2) == 0)
+                    Phase2Attack1();
+                else
+                    Phase2Attack2();
+                break;
+        }
+
+        yield return new WaitForSeconds(1.0f); // Wait for attack animation to finish
+
+        timeSinceLastAttack = Time.time;
+        isAttacking = false;
     }
+
+    // Call this function to change the boss's phase
+    public void ChangePhase(int newPhase)
+    {
+        currentPhase = newPhase;
+    }
+    private void Phase0Attack1()
+    {
+        // Execute attack 1 for phase 0
+    }
+
+    private void Phase0Attack2()
+    {
+        // Execute attack 2 for phase 0
+    }
+
+    private void Phase1Attack1()
+    {
+        // Execute attack 1 for phase 1
+    }
+
+    private void Phase1Attack2()
+    {
+        // Execute attack 2 for phase 1
+    }
+
+    private void Phase2Attack1()
+    {
+        // Execute attack 1 for phase 2
+    }
+
+    private void Phase2Attack2()
+    {
+        // Execute attack 2 for phase 2
+    }
+
 }
+
