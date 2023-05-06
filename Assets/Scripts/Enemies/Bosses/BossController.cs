8bb5ee0121eabf788759ac7471f31ae213f68598
Jerry Nordman <jerry.nordman@student.careeria.fi>
2023-05-06 16:11:47 +0300

    Started making the first phase for the boss

--- Assets/Scripts/Enemies/Bosses/BossController.cs
+++ Assets/Scripts/Enemies/Bosses/BossController.cs
@@ -9,11 +9,48 @@ public class BossController : MonoBehaviour
     private float timeSinceLastAttack = 0.0f;
     private bool isAttacking = false;
 
+    private Transform playerTransform;
+    private Rigidbody2D bossRigidbody;
+
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float attack1Range = 2.0f;
+
+    [SerializeField] private GameObject magicAttackPrefab;
+    [SerializeField] private float magicAttackRange = 10f;
+
+    private Animator animator;
+    private static readonly int IsAttacking = Animator.StringToHash("IsAttacking");
+    private static readonly int AttackType = Animator.StringToHash("AttackType");
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    private static readonly int IsCasting = Animator.StringToHash("IsCasting");
+
+
+    private void Awake()
+    {
+        bossRigidbody = GetComponent<Rigidbody2D>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+    }
     private void Update()
     {
         if (!isAttacking && Time.time >= timeSinceLastAttack + timeBetweenAttacks)
         {
             StartCoroutine(PerformAttack());
+        }
+        switch (currentPhase)
+        {
+            case 0:
+                // Move logic for phase 0
+                MoveBossPhase0();
+                break;
+            case 1:
+                // Move logic for phase 1
+                MoveBossPhase1();
+                break;
+            case 2:
+                // Move logic for phase 2
+                MoveBossPhase2();
+                break;
         }
     }
 
@@ -26,10 +63,15 @@ public class BossController : MonoBehaviour
         {
             case 0:
                 // Execute attack 1 or attack 2 for phase 0
-                if (Random.Range(0, 2) == 0)
+                float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+                if (distanceToPlayer <= attack1Range)
+                {
                     Phase0Attack1();
-                else
-                    Phase0Attack2();
+                }
+                else if (distanceToPlayer <= magicAttackRange)
+                {
+                    StartCoroutine(Phase0Attack2());
+                }
                 break;
             case 1:
                 // Execute attack 1 or attack 2 for phase 1
@@ -49,23 +91,66 @@ public class BossController : MonoBehaviour
 
         yield return new WaitForSeconds(1.0f); // Wait for attack animation to finish
 
+        // Set IsAttacking to false after the attack animation has finished
+        animator.SetBool(IsAttacking, false);
+
         timeSinceLastAttack = Time.time;
         isAttacking = false;
     }
 
-    // Call this function to change the boss's phase
-    public void ChangePhase(int newPhase)
+    private void MoveBoss(Vector2 direction)
     {
-        currentPhase = newPhase;
+        bossRigidbody.velocity = direction * moveSpeed;
+
+        // Update the Animator parameter
+        animator.SetBool(IsWalking, direction != Vector2.zero);
     }
+
+    private void MoveBossPhase0()
+    {
+        // Implement the movement logic for phase 0 here
+        Vector2 directionToPlayer = (playerTransform.position - transform.position).normalized;
+        MoveBoss(directionToPlayer);
+    }
+
+    private void MoveBossPhase1()
+    {
+        // Implement the movement logic for phase 1 here
+    }
+
+    private void MoveBossPhase2()
+    {
+        // Implement the movement logic for phase 2 here
+    }
+
+
     private void Phase0Attack1()
     {
         // Execute attack 1 for phase 0
+        float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+
+        if (distanceToPlayer <= attack1Range)
+        {
+            // Execute the melee attack animation here
+            animator.SetBool(IsAttacking, true);
+            animator.SetInteger(AttackType, 1);
+            // Deal damage to the player
+        }
     }
 
-    private void Phase0Attack2()
+    private IEnumerator Phase0Attack2()
     {
-        // Execute attack 2 for phase 0
+        // Set IsCasting to true to start the Cast animation
+        animator.SetBool(IsCasting, true);
+
+        // Wait for the Cast animation to finish
+        yield return new WaitForSeconds(1.0f); // Adjust the waiting time according to your Cast animation length
+
+        // Spawn the magic attack
+        SpawnMagicAttack(playerTransform.position);
+
+        // Set IsCasting back to false
+        animator.SetBool(IsCasting, false);
     }
 
     private void Phase1Attack1()
@@ -86,6 +171,23 @@ public class BossController : MonoBehaviour
     private void Phase2Attack2()
     {
         // Execute attack 2 for phase 2
+    }
+
+    // Call this function to change the boss's phase
+    public void ChangePhase(int newPhase)
+    {
+        currentPhase = newPhase;
+    }
+    private void SpawnMagicAttack(Vector2 position)
+    {
+        Instantiate(magicAttackPrefab, position, Quaternion.identity);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        // Draw a circle for the attack1 range
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attack1Range);
+        Gizmos.DrawWireSphere(transform.position, magicAttackRange);
     }
 
 }
